3f90cfb4e4e943feb3b5a508f9c463167192b005
Nawasawan Yenrompho <nawasawan.y@gmail.com>
2025-04-05 21:08:04 +0700

    fix: user routes

--- packages/backend/router/routes.js
+++ packages/backend/router/routes.js
@@ -5,9 +5,9 @@ import { isAuth,Role } from '../utils/Auth.js';
 const route = Router();
 
 //User route
-route.post('/SignUp',UserController.SignUp)
-route.post('/LogIn',UserController.LogIn)
-route.get('/user-info',isAuth,Role('user','admin'),UserController.UserInfo)
+route.post('/user/SignUp',UserController.SignUp)
+route.post('/user/LogIn',UserController.LogIn)
+route.get('/user/info',isAuth,Role('user','admin'),UserController.UserInfo)
 route.get('/user/history',isAuth,Role('user'),UserController.UserWithdrawalHistory)
 
 export default route
