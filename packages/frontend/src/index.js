ccd7c8e56a748237890ad7cd1cb48782048a06f4
TK <klarrrr17@gmail.com>
2025-04-05 22:22:17 +0700

    feat: delete style-components

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import "./index.css";
-import Home from "./home/home";
+import Home from "./pages/home/home";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- packages/frontend/src/pages/home/home.jsx
+++ packages/frontend/src/pages/home/home.jsx
@@ -1,11 +1,11 @@
-import Button from "../components/button/button";
+import Button from "../../components/button/button";
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page of our application.</p>
-      <Button label="button" />
+      <Button children="button" />
     </div>
   );
 }
