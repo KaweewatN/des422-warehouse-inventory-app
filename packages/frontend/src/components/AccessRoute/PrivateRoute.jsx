5ff24846e348b0a101b0903d6149bb5529e324ac
TK <klarrrr17@gmail.com>
2025-04-11 14:05:45 +0700

    fix(front): fix access control of admin

--- packages/frontend/src/components/AccessRoute/PrivateRoute.jsx
+++ packages/frontend/src/components/AccessRoute/PrivateRoute.jsx
@@ -1,11 +1,19 @@
-import React from "react";
 import {Navigate} from "react-router-dom";
 import apiService from "../../service/apiService";
 
 const PrivateRoute = ({children}) => {
   const isAuthenticated = apiService.isLogin();
+  const userRole = apiService.getUserRole();
 
-  return isAuthenticated ? children : <Navigate to="/auth" replace />;
+  if (!isAuthenticated) {
+    return <Navigate to="/auth" replace />;
+  }
+
+  if (userRole === "admin") {
+    return <Navigate to="/admin/dashboard" replace />;
+  }
+
+  return children;
 };
 
 export default PrivateRoute;
