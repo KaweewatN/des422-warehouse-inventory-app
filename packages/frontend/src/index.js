517388b6dc58cabc9ce0d74fcb985ecee7a0e33f
TK <klarrrr17@gmail.com>
2025-04-14 03:01:08 +0700

    style(front): remove navBar from auth

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import {Provider} from "./components/chakra-ui/provider";
 // Routes
@@ -15,41 +15,50 @@ import Home from "./pages/home/home";
 import Auth from "./pages/auth/Auth";
 import AdminDashboard from "./pages/admin/dashboard/AdminDashboard";
 
+const Index = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname !== "/auth" && (
+        <NavbarMenu userName="John Doe" userAvatar="https://via.placeholder.com/150" />
+      )}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <SidebarMenu userRole="user">
+                <Home />
+              </SidebarMenu>
+            </PrivateRoute>
+          }
+        />
+        <Route path="/auth" element={<Auth />} />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <AdminRoute>
+              <SidebarMenu userRole="admin">
+                <AdminDashboard />
+              </SidebarMenu>
+            </AdminRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider>
       <BrowserRouter>
-        <NavbarMenu userName="John Doe" userAvatar="https://via.placeholder.com/150" />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <SidebarMenu userRole="user">
-                  <Home />
-                </SidebarMenu>
-              </PrivateRoute>
-            }
-          />
-          <Route path="/auth" element={<Auth />} />
-          <Route
-            path="/admin/dashboard"
-            element={
-              <AdminRoute>
-                <SidebarMenu userRole="admin">
-                  <AdminDashboard />
-                </SidebarMenu>
-              </AdminRoute>
-            }
-          />
-        </Routes>
+        <Index />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
